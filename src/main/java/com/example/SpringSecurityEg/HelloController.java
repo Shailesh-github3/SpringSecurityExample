1980c2d050f1a20e8301269ac52dfe08491e1206
Shailesh-github3 <shaileshgopi812@gmail.com>
2026-01-01 20:09:42 +0530

    init commit

---
+++ src/main/java/com/example/SpringSecurityEg/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.SpringSecurityEg;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to 2026" + request.getSession().getId();
+    }
+}
---
+++ src/main/java/com/example/SpringSecurityEg/SpringSecurityEgApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityEg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityEgApplication.class, args);
+	}
+
+}
