479a31b3204441ecfaec19c58c31cc1e51f20ada
Shailesh-github3 <shaileshgopi812@gmail.com>
2026-01-02 19:57:55 +0530

    init commit - 2

---
+++ src/main/java/com/example/SpringSecurityEg/service/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.SpringSecurityEg.service;
+
+
+import com.example.SpringSecurityEg.model.UserPrincipal;
+import com.example.SpringSecurityEg.model.Users;
+import com.example.SpringSecurityEg.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Users user = repo.findByUsername(username);
+
+        if(user == null){
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("User Not Found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
